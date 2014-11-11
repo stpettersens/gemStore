8658149afb2121c34d192f1ad57a72a55b83e555
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2014-11-11 01:15:07 +0000

    Download plugins shell script (temp).

---
+++ plugins.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Install Toasty
+cordova plugin add com.chariotsolutions.toast.plugin
+
+# Install Apache InAppBrowser
+cordova plugin add https://github.com/apache/cordova-plugin-inappbrowser.git
+
