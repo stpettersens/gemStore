c2bff2ebfcde739c81f1beab71e98fdeda78931d
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2014-11-11 01:35:41 +0000

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -4,4 +4,4 @@
 cordova plugin add com.chariotsolutions.toast.plugin
 
 # Install Apache InAppBrowser
-cordova plugin add org.apache.cordova.inappbrowse
+cordova plugin add org.apache.cordova.inappbrowser
